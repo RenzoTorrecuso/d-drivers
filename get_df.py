675c77baafe3895526dcc845b2f10a2752bd905e
Katja <e.burakova@yahoo.com>
2024-03-28 21:55:08 +0100

    Polished get_df.py

--- get_df.py
+++ get_df.py
@@ -1,14 +1,20 @@
 ## For explanations see ./notebooks/Cleaning-categorising-katja.ipynb
 
 import pandas as pd
+### from timeit import timeit
 
 # Read part of the malformatted file:
-df1 = pd.read_excel('../data/data_d-drivers_2024-03-24.xlsx', sheet_name='data',
+print('=== This is the script that combines and tidies up the raw data ===')
+print('Reading the first file')
+
+df1 = pd.read_excel('data/data_d-drivers_2024-03-24.xlsx', sheet_name='data',
                     usecols=['PAGE_EFAHRER_ID', 'DATE', 'PAGE_CANONICAL_URL', 'PAGE_AUTHOR', 'WORD_COUNT', 'CLICKOUTS']
                     )
+print('Reading the second file')
 #Read everything from the new file:
-df2 = pd.read_excel('../data/data_d-drivers_2024-03-26.xlsx', sheet_name='data')
+df2 = pd.read_excel('data/data_d-drivers_2024-03-26.xlsx', sheet_name='data')
 
+print('Reading complete. \n Cleaning up the dataframes...')
 df1.columns = [col.lower() for col in df1.columns]
 df2.columns = [col.lower() for col in df2.columns]
 
@@ -36,13 +42,24 @@ df2.drop(40600, inplace=True)
 
 # Using the `left` merging: we already know that `df1` is malformatted
 key_columns = ['page_id', 'date', 'url', 'authors', 'word_count']
+
+print('Merging...')
 df = pd.merge(left=df2, right=df1, on=key_columns, how='left') 
 
 ### Imputing ###
-df = df.sort_values(['page_id', 'date', 'publishing_date', 'url'])\
+print('Imputing...')
+df = df.sort_values(['page_id', 'date', 'publish_date', 'url'])\
     .reset_index(drop=False)
 df.rename({'index': 'old_index'}, axis=1, inplace=True)
 
+# reshuffling columns
+df = df[['old_index', 'page_id', 'date', 'publish_date', 'word_count',
+         # 'publish_date_equal_to_date', # we don't need this one anymore (and never needed?)
+       'url', 'page_name', 'classification_product', 'classification_type',
+       'title', 'authors', 'daily_likes', 'daily_dislikes', 
+       'video_play', 'page_impressions', 'clickouts', 
+       'external_clicks', 'external_impressions'
+        ]]
 
 ### Including the scraped data ###
 #TODO
@@ -51,5 +68,7 @@ df.rename({'index': 'old_index'}, axis=1, inplace=True)
 #TODO
 
 ### Writing to the file ###
+print('Writing the final data frame to file')
+df.to_csv('./data/full_data.csv', encoding='utf-8', index=False)
 
-df.to_csv('./data/full_data.csv')
+print('Processing complete')
